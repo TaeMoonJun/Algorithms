6403044b0beb5bc0a1f12974680f47cffa44a5e5
Taejun Moon <junall10022@gmail.com>
2023-09-20 22:02:47 +0900

    feat: programmers 피보나치 수

---
+++ programmers/피보나치수.java
@@ -0,0 +1,13 @@
+//12945
+class 피보나치수 {
+    public int solution(int n) {
+        int[] fibo = new int[n+1];
+        fibo[1] = 1;
+
+        for(int i=2;i<n+1;i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2])%1234567;
+        }
+
+        return fibo[n];
+    }
+}
